7c837007381943fa88179fddc1fa113f322c874a
Emilio Platzer <emilioplatzer@gmail.com>
2023-04-28 20:03:53 -0300

    Quito dependencias innecesarias

--- src/server/app-gestik.ts
+++ src/server/app-gestik.ts
@@ -62,27 +62,13 @@ export class AppGestik extends AppBackend{
         return {menu:menuContent};
     }
     clientIncludes(req:Request|null, opts:OptsClientPage):ClientModuleDefinition[]{
-        var UsandoREact = true;
         var menuedResources:ClientModuleDefinition[]=req && opts && !opts.skipMenu ? [
             { type:'js' , src:'client.js' },
         ]:[
             {type:'js' , src:'unlogged.js' },
         ];
         var list: ClientModuleDefinition[] = [
-            ...(UsandoREact?[
-                { type: 'js', module: 'react', modPath: 'umd', fileDevelopment:'react.development.js', file:'react.production.min.js' },
-                { type: 'js', module: 'react-dom', modPath: 'umd', fileDevelopment:'react-dom.development.js', file:'react-dom.production.min.js' },
-                { type: 'js', module: '@mui/material', modPath: 'umd', fileDevelopment:'material-ui.development.js', file:'material-ui.production.min.js' },
-                { type: 'js', module: 'material-styles', fileDevelopment:'material-styles.development.js', file:'material-styles.production.min.js' },
-                { type: 'js', module: 'clsx', file:'clsx.min.js' },
-                { type: 'js', module: 'redux', modPath:'../dist', fileDevelopment:'redux.js', file:'redux.min.js' },
-                { type: 'js', module: 'react-redux', modPath:'../dist', fileDevelopment:'react-redux.js', file:'react-redux.min.js' },
-            ]:[]) satisfies ClientModuleDefinition[],
             ...super.clientIncludes(req, opts),
-            ...(UsandoREact?[
-                { type: 'js', module: 'redux-typed-reducer', modPath:'../dist', file:'redux-typed-reducer.js' },
-                { type: 'js', src: 'adapt.js' },
-            ]:[])  satisfies ClientModuleDefinition[],
             { type: 'css', file: 'menu.css' },
             ... menuedResources
         ] satisfies ClientModuleDefinition[];
