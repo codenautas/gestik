5cc69bcee420fc9f10846c75d4f63e913f55c758
Emilio Platzer <emilioplatzer@gmail.com>
2023-04-28 20:29:52 -0300

    agrego columnas SFK en proyectos estados

--- src/server/table-proyectos_estados.ts
+++ src/server/table-proyectos_estados.ts
@@ -11,11 +11,17 @@ export function proyectos_estados():TableDefinition{
         fields: [
             {name: 'proyecto', typeName: 'text'},
             {name: 'estado', typeName: 'text'},
-            {name: 'cant_tickets', typeName: 'bigint', inTable:false,},
+            {name: 'cant_tickets', typeName: 'bigint'},
+            // equivale a:
+            // {name: 'estados__orden', typeName: 'integer', title:'orden'},
+            // {name: 'estados__solapa', typeName: 'integer', title:'solapa'},
         ],
         primaryKey: ['proyecto', 'estado'],
+        softForeignKeys: [
+            {references: 'estados', fields: ['estado'], displayFields: ['solapa', 'orden'], displayAfterFieldName: 'cant_tickets'}
+        ],
         detailTables: [
-            {table: 'tickets', fields:['proyecto', 'estado'], abr:'t'}
+            {table: 'tickets', fields: ['proyecto', 'estado'], abr: 't'}
         ],
         sql: {
             isTable: false,
@@ -27,6 +33,7 @@ export function proyectos_estados():TableDefinition{
                 ) AS t ON true
             )`
         },
+        sortColumns: [{column: 'estados__orden'}, {column: 'estado'}, {column: 'proyecto'}]
     }
     return td
 }
