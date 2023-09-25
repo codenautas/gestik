a5c82be1c96955bff20852f75b8e1c95c50b921b
Sebastián <spierini@info.lifia.unlp.edu.ar>
2023-09-25 09:49:10 -0300

    fix: se agrega istable:false para que corra el dump

--- src/server/table-tickets_equipos_usuarios.ts
+++ src/server/table-tickets_equipos_usuarios.ts
@@ -6,7 +6,10 @@ import { tickets } from "./table-tickets";
 export function tickets_equipos_usuarios(context:TableContext):TableDefinition{
     const td = tickets(context);
     td.name = 'tickets_equipos_usuarios';
-    td.sql && (td.sql.where = `tickets.requirente <> '${context.user.usuario}' and exists (select * from equipos_usuarios eu where eu.usuario = tickets.requirente and 
-        eu.equipo in (select e.equipo from equipos_usuarios e where e.usuario = '${context.user.usuario}'))`);
+    td.sql && (
+        td.sql.isTable=false, 
+        td.sql.where = `tickets.requirente <> '${context.user.usuario}' and exists (select * from equipos_usuarios eu where eu.usuario = tickets.requirente and 
+        eu.equipo in (select e.equipo from equipos_usuarios e where e.usuario = '${context.user.usuario}'))`
+    );
     return td
 }
