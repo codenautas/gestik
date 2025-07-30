07f424e26fef54f0dbb17ed9f7f4e61f4ab664c0
sebastian pierini <seba.i.pierini@gmail.com>
2025-07-30 20:53:47 -0300

    se arregla trigger de proyectos cerrados

--- install/no_editar_proyectos_cerrados_trg.sql
+++ install/no_editar_proyectos_cerrados_trg.sql
@@ -1,30 +1,40 @@
-SET search_path = 'gestik';
-CREATE OR REPLACE FUNCTION no_editar_proyectos_cerrados()
-RETURNS trigger
-LANGUAGE plpgsql
+set search_path = gestik;
+CREATE OR REPLACE FUNCTION no_editar_proyectos_cerrados_trg()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
 AS $BODY$
 DECLARE
     v_abierto boolean;
 BEGIN
    if tg_op = 'INSERT' THEN
-      select abierto = true into v_abierto from proyectos where proyecto = new.proyecto;
-      IF v_abierto = false AND tg_op = 'INSERT' THEN
+      select abierto is true into v_abierto from proyectos where proyecto = new.proyecto;
+      IF v_abierto = false THEN
          RAISE EXCEPTION 'No se pueden insertar tickets en proyectos cerrados.';
       end if;
    end if;
-   if tg_op = 'DELETE' OR tg_op = 'UPDATE' THEN
-      select abierto = true into v_abierto from proyectos where proyecto = old.proyecto;
-   	  if v_abierto = false AND tg_op = 'DELETE' THEN
-         RAISE EXCEPTION 'No se pueden eliminar tickets de proyectos cerrados.';
-	  end if;
-	  if v_abierto = false AND tg_op = 'UPDATE' THEN
+   if tg_op = 'UPDATE' THEN
+      select abierto is true into v_abierto from proyectos where proyecto = old.proyecto;
+      if v_abierto = false THEN
          RAISE EXCEPTION 'No se pueden actualizar tickets de proyectos cerrados.';
       end if;
+	  select abierto is true into v_abierto from proyectos where proyecto = new.proyecto;
+      if v_abierto = false THEN
+         RAISE EXCEPTION 'No se pueden actualizar tickets de proyectos cerrados.';
+      end if;
+   end if;
+   if tg_op = 'DELETE' THEN
+      select abierto is true into v_abierto from proyectos where proyecto = old.proyecto;
+   	  if v_abierto = false THEN
+         RAISE EXCEPTION 'No se pueden eliminar tickets de proyectos cerrados.';
+	  end if;
+	  RETURN old;
    end if;
    RETURN new;
 END;
 $BODY$;
-CREATE TRIGGER trg_no_editar_proyectos_cerrados
-BEFORE INSERT OR DELETE OR UPDATE ON gestik.tickets
-FOR EACH ROW
-EXECUTE FUNCTION no_editar_proyectos_cerrados();
+
+CREATE TRIGGER no_editar_proyectos_cerrados_trg
+    BEFORE INSERT OR DELETE OR UPDATE
+    ON tickets
+    FOR EACH ROW
+    EXECUTE FUNCTION no_editar_proyectos_cerrados_trg();
