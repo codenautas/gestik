1a0aba3d7dc590e0a64c73aa4d24649a6e64e3a1
sebastian pierini <seba.i.pierini@gmail.com>
2025-08-06 17:51:18 -0300

    se arregla trigger para proyectos cerrados

--- install/no_editar_proyectos_cerrados_trg.sql
+++ install/no_editar_proyectos_cerrados_trg.sql
@@ -1,40 +1,37 @@
 set search_path = gestik;
 CREATE OR REPLACE FUNCTION no_editar_proyectos_cerrados_trg()
-    RETURNS trigger
-    LANGUAGE 'plpgsql'
+  RETURNS trigger
+  LANGUAGE 'plpgsql'
 AS $BODY$
 DECLARE
-    v_abierto boolean;
+  v_es_proyecto_abierto boolean;
+  v_cant_proyecto_abierto integer;
 BEGIN
-   if tg_op = 'INSERT' THEN
-      select abierto is true into v_abierto from proyectos where proyecto = new.proyecto;
-      IF v_abierto = false THEN
-         RAISE EXCEPTION 'No se pueden insertar tickets en proyectos cerrados.';
-      end if;
-   end if;
-   if tg_op = 'UPDATE' THEN
-      select abierto is true into v_abierto from proyectos where proyecto = old.proyecto;
-      if v_abierto = false THEN
-         RAISE EXCEPTION 'No se pueden actualizar tickets de proyectos cerrados.';
-      end if;
-	  select abierto is true into v_abierto from proyectos where proyecto = new.proyecto;
-      if v_abierto = false THEN
-         RAISE EXCEPTION 'No se pueden actualizar tickets de proyectos cerrados.';
-      end if;
-   end if;
-   if tg_op = 'DELETE' THEN
-      select abierto is true into v_abierto from proyectos where proyecto = old.proyecto;
-   	  if v_abierto = false THEN
-         RAISE EXCEPTION 'No se pueden eliminar tickets de proyectos cerrados.';
-	  end if;
-	  RETURN old;
-   end if;
-   RETURN new;
+  if tg_op = 'INSERT' THEN
+    select abierto is true into v_es_proyecto_abierto from proyectos where proyecto = new.proyecto;
+    IF v_es_proyecto_abierto = false THEN
+      RAISE EXCEPTION 'No se pueden insertar tickets en proyectos cerrados.';
+    end if;
+  end if;
+  if tg_op = 'UPDATE' THEN
+    select count(*) into v_cant_proyecto_abierto from gestik.proyectos where (proyecto = old.proyecto or proyecto = new.proyecto) and abierto is false LIMIT 1;
+    if v_cant_proyecto_abierto > 0 THEN
+      RAISE EXCEPTION 'No se pueden actualizar tickets de proyectos cerrados.';
+    end if;
+  end if;
+  if tg_op = 'DELETE' THEN
+    select abierto is true into v_es_proyecto_abierto from proyectos where proyecto = old.proyecto;
+    if v_es_proyecto_abierto = false THEN
+      RAISE EXCEPTION 'No se pueden eliminar tickets de proyectos cerrados.';
+    end if;
+    RETURN old;
+  end if;
+  RETURN new;
 END;
 $BODY$;
 
 CREATE TRIGGER no_editar_proyectos_cerrados_trg
-    BEFORE INSERT OR DELETE OR UPDATE
-    ON tickets
-    FOR EACH ROW
-    EXECUTE FUNCTION no_editar_proyectos_cerrados_trg();
+  BEFORE INSERT OR DELETE OR UPDATE
+  ON tickets
+  FOR EACH ROW
+  EXECUTE FUNCTION no_editar_proyectos_cerrados_trg();
--- operaciones/actualizar2025-03-07.sql
+++ operaciones/actualizar2025-03-07.sql
@@ -1,3 +1,4 @@
 alter table gestik.proyectos add column "abierto" boolean default 'true';
+alter table gestik.proyectos alter column "abierto" set not null;
 alter table gestik.proyectos add column "observaciones" text;
 alter table gestik.proyectos add column "descripcion" text;
