2a77d4a22418af976eceef63967e4427f2cdd76a
sebastian pierini <seba.i.pierini@gmail.com>
2025-09-23 12:27:52 -0300

    Se actualiza politicas para tickets

--- src/server/table-tickets.ts
+++ src/server/table-tickets.ts
@@ -2,24 +2,63 @@
 
 import { TableContext, TableDefinition, FieldDefinition } from "./types-gestik";
 
-const sqlPoliticasTickets = (modo : 'edit' | 'view') => `( 
-        SELECT rol='admin' FROM usuarios WHERE usuario = get_app_user()
-    ) OR (
+const sqlView = `
+    (
+		requirente = get_app_user()
+	) OR (
+		asignado = get_app_user()
+	) OR (
+        SELECT true
+        FROM equipos_usuarios eu
+        JOIN equipos_usuarios et ON (eu.equipo = et.equipo)
+		JOIN equipos_proyectos ep ON (eu.equipo = ep.equipo)
+        WHERE eu.usuario = get_app_user() AND ep.proyecto = tickets.proyecto AND ((et.usuario = tickets.requirente) OR (et.usuario = tickets.asignado))
+        LIMIT 1
+	) OR (
+		SELECT true
+		FROM equipos_proyectos ep
+		JOIN equipos_usuarios eu ON (eu.equipo = ep.equipo)
+		WHERE eu.usuario = get_app_user() AND ep.proyecto = tickets.proyecto and ep.es_asignado is not true and ep.es_requirente is not true
+		LIMIT 1
+	)`;
+
+const sqlEsRequirente = `
+    (
+        SELECT COALESCE(ep.es_requirente, false)
+        FROM equipos_proyectos ep
+        JOIN equipos_usuarios eu ON (eu.equipo = ep.equipo)
+        WHERE eu.usuario = get_app_user() AND ep.proyecto = tickets.proyecto
+        LIMIT 1
+    ) AND (
         requirente = get_app_user()
-    ) OR ( 
+    )`;
+
+const sqlEsAsignado = `
+    (
+        SELECT COALESCE(ep.es_asignado, false)
+        FROM equipos_proyectos ep
+        JOIN equipos_usuarios eu ON (eu.equipo = ep.equipo)
+        WHERE eu.usuario = get_app_user() AND ep.proyecto = tickets.proyecto
+        LIMIT 1
+    ) AND (
         asignado = get_app_user()
+    )`;
+
+const sqlPoliticasTickets = (modo : 'delete' | 'insert' | 'update' | 'select') => {
+    let specificPolicy: string;
+    switch (modo) {
+        case 'select': specificPolicy = sqlView; break;
+        case 'insert': specificPolicy = sqlEsRequirente; break;
+        case 'update': specificPolicy = `(${sqlEsRequirente}) OR (${sqlEsAsignado})`; break;
+        case 'delete': specificPolicy = `(${sqlEsRequirente}) OR (${sqlEsAsignado})`; break;
+        default: specificPolicy = 'false'; break;
+    }
+    return `(
+        SELECT rol = 'admin' FROM usuarios WHERE usuario = get_app_user()
     ) OR (
-        SELECT true 
-            FROM equipos_usuarios eu INNER JOIN equipos_usuarios et ON eu.equipo = et.equipo
-            WHERE eu.usuario = get_app_user()
-                AND (et.usuario = tickets.requirente OR et.usuario = tickets.asignado) limit 1
-    ) OR (
-        SELECT ${modo == 'view' ? 'true' : '(ep.es_requirente OR ep.es_asignado)'}
-            FROM equipos_usuarios eu INNER JOIN equipos_proyectos ep ON eu.equipo = ep.equipo
-            WHERE eu.usuario = get_app_user()
-                AND ep.proyecto = tickets.proyecto limit 1
-    )
-`;
+        ${specificPolicy}
+    )`;
+};
 
 export function sqlExprCantTickets(_context: TableContext, filter: string, joinEstados?:boolean){
     return `(SELECT nullif(count(*), 0) as cant_tickets FROM tickets t
@@ -37,7 +76,7 @@ export function tickets(_context: TableContext, opts: Opts = {}):TableDefinition
         {name:'proyecto'           , typeName:'text'  , zona:'1' ,siempre:true , },
         {name:'ticket'             , typeName:'bigint', zona:'1' ,siempre:true , nullable:true, editable:false, defaultDbValue:'0'},
         {name:'tipo_ticket'        , typeName:'text'  , zona:'1' , title:'tipo ticket'},
-        {name:'cant_anotaciones'   , typeName:'bigint', zona:'0' , inTable:false, editable:false, title:'c.a.', description: 'cantidad anotaciones'}, 
+        {name:'cant_anotaciones'   , typeName:'bigint', zona:'0' , inTable:false, editable:false, title:'c.a.', description: 'cantidad anotaciones'},
         {name:'asunto'             , typeName:'text'  , zona:'1' , title:'asunto', nullable:false},
         {name:'descripcion'        , typeName:'text'  , zona:'2' , title:'descripción' },
         {name:'requirente'         , typeName:'text'  , zona:'3' , specialDefaultValue: 'current_user'},
@@ -80,23 +119,24 @@ export function tickets(_context: TableContext, opts: Opts = {}):TableDefinition
         ],
         sql:{
             isTable,
-            fields:{ 
+            fields:{
                 cant_anotaciones:{ expr: `(SELECT nullif(count(*),0) FROM anotaciones a WHERE a.proyecto = tickets.proyecto and a.ticket = tickets.ticket)` },
                 asignado_pendiente:{ expr:`(CASE WHEN estados.esta_pendiente THEN tickets.asignado ELSE null END)`}
             },
             ...(isTable ? {
                 policies: {
                     update: {
-                        using: sqlPoliticasTickets('edit'),
+                        using: sqlPoliticasTickets('update'),
+                        check: sqlPoliticasTickets('update'),
                     },
                     delete: {
-                        using: sqlPoliticasTickets('edit'),
+                        using: sqlPoliticasTickets('delete'),
                     },
                     select: {
-                        using: sqlPoliticasTickets('view'),
+                        using: sqlPoliticasTickets('select'),
                     },
                     insert:{
-                        check: sqlPoliticasTickets('edit'),
+                        check: sqlPoliticasTickets('insert'),
                     }
                 },
             } : {})
